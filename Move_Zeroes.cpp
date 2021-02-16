ff797999c518a1d0a00f0f455158a84a3b934b2f
Dasol Kim <zoocanon92@gmail.com>
2021-02-16 22:35:50 +0900

    Move Zeroes

---
+++ Move_Zeroes.cpp
@@ -0,0 +1,20 @@
+// Problem from https://leetcode.com/problems/move-zeroes/
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (;j < nums.size(); j++) {
+            nums[j] = 0;
+        }        
+    }
+};
